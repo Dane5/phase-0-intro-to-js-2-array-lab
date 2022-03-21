4accbbbce9817c8ca4b99519f13d041dd76adfc9
Paschal Ramdan <dane.ramdan@gmail.com>
2022-03-21 11:45:45 -0400

    commit tree 89d4ddadc81cddeccf543871faf38b91fb59d8be

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat (name) {
+    cats.push(name);
+}
+
+
+function destructivelyPrependCat (name) {
+    cats.unshift(name);
+  }
+  
+  function destructivelyRemoveLastCat () {
+    cats.pop();
+  }
+  
+  function destructivelyRemoveFirstCat () {
+    cats.shift();
+  }
+  
+  function appendCat (name) {
+    return [...cats, name];
+  }
+  
+  function prependCat (name) {
+    return [name, ...cats];
+  }
+  
+  function removeFirstCat () {
+    return cats.slice(1);
+  }
+  
+  function removeLastCat () {
+    return cats.slice(0, cats.length - 1);
+  }
+
+
+
+
+// describe('destructivelyAppendCat(name)', function () {
+//     it('appends a cat to the end of the cats array', function () {
+//       destructivelyAppendCat('Ralph');
+
+//       expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield", "Ralph"]);
+//     });
+//   });
